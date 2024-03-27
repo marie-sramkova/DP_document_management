bfd3164a16f39ba261bc4f93cf0e3ce88f2e05a9
Marie <P22085@student.osu.cz>
2024-03-27 17:57:47 +0100

    Entities for mapping, working with files, count of new files

---
+++ wpf_prototype/WpfPrototype/WpfPrototype/additionalLogic/FileEditor.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using WpfPrototype.additionalLogic.entities;
+
+namespace WpfPrototype
+{
+    public class FileEditor
+    {
+
+        private FileEditor() {
+            String fileText = "";
+            if (!File.Exists(UserSettings.settingsFilePath))
+            {
+                File.Create(UserSettings.settingsFilePath);
+                
+            }
+            fileText = File.ReadAllText(UserSettings.settingsFilePath);
+            Console.WriteLine(fileText);
+            SettingsEntity = JsonConvert.DeserializeObject<SettingsEntity>(fileText);
+        }
+
+
+        private static FileEditor instance = null;
+        public static FileEditor Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new FileEditor();
+                }
+                return instance;
+            }
+        }
+
+        public SettingsEntity SettingsEntity { get; }
+
+        public void AddNewFiles(List<String> files)
+            {
+                foreach (var file in files)
+                {
+                    List<DocAttribute> docAttributes = new List<DocAttribute>();
+                    //todo: convert attributes from analyzed file to entities
+                    DocFile newDoc = new DocFile(file, docAttributes);
+                }
+            //todo: save to file
+            }
+
+        public Boolean CheckIfSettingsFileIsEmpty()
+        {
+            if (SettingsEntity == null) { 
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ wpf_prototype/WpfPrototype/WpfPrototype/additionalLogic/entities/SettingsEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPrototype.additionalLogic.entities
+{
+    public class SettingsEntity
+    {
+        public List<Template> Templates {  get; set; }
+        public List<DocFile> DocFiles { get; set; }
+
+        public void AddDoc(DocFile docFile)
+        {
+            DocFiles.Add(docFile);
+        }
+
+    }
+}
