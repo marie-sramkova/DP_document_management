4a7d268b518a10483c13af9c70ad94ef3fafa153
Marie <P22085@student.osu.cz>
2024-05-13 11:22:03 +0200

    Tesseract path corrected for release

--- wpf_prototype/WpfPrototype/WpfPrototype/additionalLogic/ocr/TesseractOCR.cs
+++ wpf_prototype/WpfPrototype/WpfPrototype/additionalLogic/ocr/TesseractOCR.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Tesseract;
@@ -14,12 +15,19 @@ namespace WpfPrototype.additionalLogic.ocr
     {
         public string GetTextFromImage(byte[] bytes)
         {
-            var path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(System.AppDomain.CurrentDomain.BaseDirectory).ToString()).ToString()).ToString()).ToString() + "\\tessdata";
+            //System.Diagnostics.Debugger.Launch();
+            //var path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(System.AppDomain.CurrentDomain.BaseDirectory).ToString()).ToString()).ToString()).ToString() + "/tessdata";
+
+            //var path = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(System.AppDomain.CurrentDomain.BaseDirectory).ToString()).ToString()).ToString()).ToString();
+            //var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            path = Path.Combine(path, "tessdata");
+            Debug.WriteLine(path);
             using (var engine = new TesseractEngine(path, "ces", EngineMode.Default))
             {
                 using (var img = Pix.LoadFromMemory(bytes))
                 {
-                    // engine.SetVariable("tessedit_char_whitelist", "0123456789");
+                    //engine.SetVariable("tessedit_char_whitelist", "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ");
                     using (var page = engine.Process(img))
                     {
                         var text = page.GetText();
