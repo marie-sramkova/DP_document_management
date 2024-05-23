14c5f8143e6cde2cc5c2fd378a6c78b103b5a493
Marie <P22085@student.osu.cz>
2024-05-23 18:58:24 +0200

    Packages renamed

---
+++ app/app/DocumentManagementApp/additionalLogic/UserSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentManagementApp.additionalLogic.entities;
+
+namespace DocumentManagementApp
+{
+    public class UserSettings
+    {
+        
+        //todo: change in settings
+        public static string directoryPath = "";
+        public static string settingsDocumentsFilePath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "/data/DocumentManagementApp" + "/settings_documents.txt";
+        public static string settingsTemplatesFilePath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "/data/DocumentManagementApp" + "/settings_templates.txt";
+
+    }
+}
